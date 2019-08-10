ab1b537dd2b9ad8d8cf2da55884afa68e82afc6e
Matthias Wirth <matthias.wirth@gmail.com>
2019-08-10 13:20:48 +0200

    remove stuff that was moved to the scripts repository

--- uninstall-complete.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/adsb-wiki/master/uninstall-dump1090-fa.sh)"
-bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/adsb-wiki/master/uninstall-dump1090-fa-autogain.sh)"
-bash -c "$(wget -q -O - https://raw.githubusercontent.com/wiedehopf/dump1090-retro-html/master/uninstall.sh)"
